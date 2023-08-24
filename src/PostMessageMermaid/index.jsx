f1b5b298b769cad7ec97a9f58e8f510747088d94
pwamly <pwamly2016@gmail.com>
2023-08-24 11:51:37 +0300

    thus state

--- src/PostMessageMermaid/index.jsx
+++ src/PostMessageMermaid/index.jsx
@@ -1,33 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { getPost } from 'mattermost-redux/selectors/entities/posts';
-import Mermaid from 'react-mermaid';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { getPost } from 'mattermost-redux/selectors/entities/posts';
+// import Mermaid from 'react-mermaid';
 
-const PostMessageMermaid = ({ mermaidData }) => {
-  if (!mermaidData) {
-    return null;
-  }
-  const rnd = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-  const name = "diagram"+rnd;
-  return (
-    <Mermaid name={name}>{mermaidData}</Mermaid>
-  );
-};
+// const PostMessageMermaid = ({ mermaidData }) => {
+//   // if (!mermaidData) {
+//   //   return null;
+//   // }
+//   // const rnd = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+//   // const name = "diagram"+rnd;
+//   return (
+//     <Mermaid name='pwamly'>helo guys</Mermaid>
+//   );
+// };
 
-const mapStateToProps = (state, ownProps) => {
-  const postId = ownProps.postId.replace("user-activity-", "");
-  const post = getPost(state, postId);
+// const mapStateToProps = (state, ownProps) => {
 
-  try {
-    if (post.props && post.props.mermaidData) {
-      return { mermaidData: post.props.mermaidData };
-    }
-  } catch (e) {}
 
-  return {};
-};
 
-export default connect(
-  mapStateToProps,
-  null
-)(PostMessageMermaid);
+//   return {};
+// };
+
+// export default connect(mapStateToProps,null)(PostMessageMermaid);
+// // export default PostMessageMermaid;
