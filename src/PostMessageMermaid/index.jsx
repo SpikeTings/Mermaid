b0a77f5f3257f6c3b3065f33f4a204dec0f3d113
pwamly <pwamly2016@gmail.com>
2023-08-31 17:14:23 +0300

    upgrading mermaid plugin

--- src/PostMessageMermaid/index.jsx
+++ src/PostMessageMermaid/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getPost } from 'mattermost-redux/selectors/entities/posts';
-import Mermaid from 'react-mermaid';
+import Mermaid from "react-mermaid2";
 
 const PostMessageMermaid = ({ mermaidData }) => {
   if (!mermaidData) {
@@ -10,19 +10,14 @@ const PostMessageMermaid = ({ mermaidData }) => {
   const rnd = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
   const name = "diagram"+rnd;
   return (
-    <Mermaid name={name}>{mermaidData}</Mermaid>
-  );
+    <Mermaid chart={mermaidData}/>  );
 };
 
 const mapStateToProps = (state, ownProps) => {
   const postId = ownProps.postId.replace("user-activity-", "");
   const post = getPost(state, postId);
 
-  console.log('teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeest');
-
   try {
-    console.log('rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr',post.props);
-
     if (post.props && post.props.mermaidData) {
       return { mermaidData: post.props.mermaidData };
     }
